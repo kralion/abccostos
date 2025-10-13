bde41e67a8a6a74fc90d2fd1a11ce105284cc9e7
Brayan Paucar <joan300501@gmail.com>
2025-10-13 17:40:38 -0500

    new tabs styling

--- apps/presupuestos/src/features/presupuesto/components/secondarytabs.tsx
+++ apps/presupuestos/src/features/presupuesto/components/secondarytabs.tsx
@@ -175,7 +175,7 @@ export default function SecondaryTabs({
           <TabsTrigger
             key={tab.value}
             value={tab.value}
-            className='group hover:bg-accent hover:text-foreground data-[state=active]:after:bg-primary data-[state=active]:hover:bg-accent relative after:absolute after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5 data-[state=active]:bg-transparent data-[state=active]:shadow-none'
+            className='group hover:bg-accent text-muted-foreground data-[state=active]:after:bg-primary data-[state=active]:hover:bg-accent relative after:absolute after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5 data-[state=active]:bg-transparent data-[state=active]:text-black data-[state=active]:shadow-none'
           >
             {tab.icon}
             <span className='hidden group-data-[state=active]:inline md:inline'>
